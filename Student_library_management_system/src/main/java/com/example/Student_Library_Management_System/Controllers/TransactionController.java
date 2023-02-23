7ba311395ec6776f722fbeaa6a317a777fc23b7d
saurabhsngh5484 <saurabhsngh5484@gmail.com>
2023-02-23 11:24:02 +0530

    first commit

---
+++ Student_library_management_system/src/main/java/com/example/Student_Library_Management_System/Controllers/TransactionController.java
@@ -0,0 +1,34 @@
+package com.example.Student_Library_Management_System.Controllers;
+
+
+import com.example.Student_Library_Management_System.DTOs.IssueBookRequestDto;
+import com.example.Student_Library_Management_System.Services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+
+    @Autowired
+    TransactionService transactionService;
+
+    @PostMapping("issueBook")
+    public String issueBook(@RequestBody IssueBookRequestDto issueBookRequestDto){
+
+        try{
+            return transactionService.issueBook(issueBookRequestDto);
+        }catch (Exception e){
+            return e.getMessage();
+        }
+
+    }
+
+    @GetMapping("/getTxnInfo")
+    public String getTransactionEntry(@RequestParam("bookId")Integer bookId,@RequestParam
+            ("cardId")Integer cardId){
+
+        return transactionService.getTransactions(bookId,cardId);
+    }
+}
